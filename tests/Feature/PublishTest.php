630efef7d6feec5a7bc1ca7a692f8a9d88a11a42
FabioGuin <info@starzero.it>
2024-07-09 18:14:28 +0200

    Add publish unit test

---
+++ tests/Feature/PublishTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FabioGuin\LivewireSearchableSelect\Tests\Unit;
+
+use FabioGuin\LivewireSearchableSelect\Tests\TestCase;
+
+class PublishTest extends TestCase
+{
+    /** @test */
+    public function it_publishes_the_component()
+    {
+        $this->artisan('vendor:publish', ['--provider' => 'FabioGuin\LivewireSearchableSelect\LivewireSearchableSelectServiceProvider'])
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function it_publishes_the_view()
+    {
+        $this->artisan('vendor:publish', [
+            '--provider' => 'FabioGuin\LivewireSearchableSelect\LivewireSearchableSelectServiceProvider',
+            '--tag' => 'views'
+        ])->assertExitCode(0);
+    }
+
+    /** @test */
+    public function it_publishes_the_config()
+    {
+        $this->artisan('vendor:publish', [
+            '--provider' => 'FabioGuin\LivewireSearchableSelect\LivewireSearchableSelectServiceProvider',
+            '--tag' => 'config'
+        ])->assertExitCode(0);
+    }
+
+    /** @test */
+    public function it_publishes_the_translations()
+    {
+        $this->artisan('vendor:publish', [
+            '--provider' => 'FabioGuin\LivewireSearchableSelect\LivewireSearchableSelectServiceProvider',
+            '--tag' => 'lang'
+        ])->assertExitCode(0);
+    }
+}
